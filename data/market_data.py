40af1cdc1e662fb6802a9a9161b3369252c7677f
Kushagra Agarwal <kushagraagarwal@Kushagras-MacBook-Pro.local>
2026-04-17 17:56:25 +0530

    Refactor market_data and implied_vol

--- data/market_data.py
+++ data/market_data.py
@@ -1,70 +1,44 @@
 from datetime import datetime as dt
-
 import yfinance as yf
-from scipy.optimize import brentq
-import numpy as np
-import pandas as pd
-import plotly.express as px
-
-from pricing.black_scholes import EuropeanOption
-
-TODAY = dt.today()
-
-ticker = yf.Ticker("SPY") # State Street SPDR S&P 500 ETF Trust
-expiries = ticker.options # Option expiries available
-
-date_list = []
-
-for expiry in expiries:
-    day_diff = (dt.strptime(expiry, "%Y-%m-%d") - TODAY).days
-    if 70 <= day_diff <= 90: # Option expiry between 70 and 90 days from now
-        date_list.append(expiry)
-
-date = date_list[0]
-option_chain = ticker.option_chain(date)
-
-spot = ticker.history(period="1d")["Close"].iloc[-1] # Spot price of underlying: S in Black Scholes model
-print(f"Spot price: ${spot:.2f}")
-
-calls = option_chain.calls[["strike", "lastPrice", "ask", "bid", "impliedVolatility"]]
-calls = calls[ (0.85 * spot <= calls["strike"]) & (calls["strike"] <= 1.15 * spot) ] # Filtering out stale prices
-calls.reset_index(drop=True, inplace=True)
-
-print(f"\nCalls:\n{calls}")
-
-tnx = yf.Ticker("^IRX")
-interest_rate = tnx.history(period="1d")["Close"].iloc[-1]/100
-print(f"Current 13 Week Treasury Yield: {interest_rate}") # US 13 Week Treasury Bill: r in the Black Scholes model
-
-time_to_expiry = (dt.strptime(date, "%Y-%m-%d") - TODAY).days/365 # Time to expiry in years: T in Black Scholes Model
-print(f"Time to expiry: {time_to_expiry * 365:.0f} days\n")
-
-zero_bid_ask = calls[ (calls["ask"] == 0) | (calls["bid"] == 0) ]
-if len(zero_bid_ask) >= 0.5 * len(calls):
-    calls["marketPrice"] = calls["lastPrice"]               # Set last price as the market price in case of illiquid options
-else:
-    calls["marketPrice"] = calls["ask"].add(calls["bid"])/2 # Otherwise set them to ask-bid midpoint
-
-# Cleaning data
-calls = calls[calls["marketPrice"] > 0]
-calls = calls.dropna(subset=["marketPrice"])
 
 
-def helper(implied_vol, row):
-    option = EuropeanOption(spot, row.strike, time_to_expiry, interest_rate, implied_vol)
-    return option.call_price() - row.marketPrice
+class MarketData:
+    def __init__(self, ticker: str):
+        self.today = dt.today()
+        self.ticker = yf.Ticker(ticker)
+        expiries = self.ticker.options  # Option expiries available
 
+        self.expiries = []
+        for expiry in expiries:
+            day_diff = (dt.strptime(expiry, "%Y-%m-%d") - self.today).days
+            if 30 <= day_diff <= 180:   # Option expiry between 30 days and 180 days from now
+                self.expiries.append(expiry)
 
-strike_vol = []
-for _, data_row in calls.iterrows():
-    try:
-        imp_vol = brentq(f=helper, a=1e-9, b=5, args=(data_row, )) # Solve for root of helper
-        strike_vol.append((data_row.strike, imp_vol))
-    except ValueError:
-        strike_vol.append((data_row.strike, np.nan)) # When no solution exists
+        # Spot price of underlying: S in Black Scholes model
+        self.spot = self.ticker.history(period="1d")["Close"].iloc[-1]
 
-strike_vol_df = pd.DataFrame(strike_vol, columns=["strike", "implied_vol"])
-strike_vol_df = strike_vol_df.dropna().reset_index(drop=True)
+        tnx = yf.Ticker("^IRX")
+        # US 13 Week Treasury Bill: r in the Black Scholes model
+        self.interest_rate = tnx.history(period="1d")["Close"].iloc[-1] / 100
 
-plot = px.line(data_frame=strike_vol_df, x="strike", y="implied_vol")
-plot.show()
+    def get_calls(self, date: str):
+        if date not in self.expiries:
+            raise ValueError("Date must be in the list of expiries")
+
+        option_chain = self.ticker.option_chain(date)
+
+        calls = option_chain.calls[["strike", "lastPrice", "ask", "bid"]]
+        calls = calls[ (0.85 * self.spot <= calls["strike"]) &
+                       (calls["strike"] <= 1.15 * self.spot) ] # Filtering out stale prices
+        calls.reset_index(drop=True, inplace=True)
+
+        zero_bid_ask = calls[ (calls["ask"] == 0) | (calls["bid"] == 0) ]
+        if len(zero_bid_ask) >= 0.5 * len(calls):
+            calls["marketPrice"] = calls["lastPrice"]  # Set last price as the market price in case of illiquid options
+        else:
+            calls["marketPrice"] = calls["ask"].add(calls["bid"])/2  # Otherwise set them to ask-bid midpoint
+
+        # Cleaning data
+        calls = calls[calls["marketPrice"] > 0]
+        calls = calls.dropna(subset=["marketPrice"])
+        return calls[["strike", "marketPrice"]]
---
+++ vol_surface/implied_vol.py
@@ -0,0 +1,40 @@
+from datetime import datetime as dt
+
+from scipy.optimize import brentq
+import numpy as np
+import pandas as pd
+# import plotly.express as px
+
+from pricing.black_scholes import EuropeanOption
+from data.market_data import MarketData
+
+ticker = "SPY"
+
+
+def get_strike_vol_df(market_data: MarketData):
+    """
+    Takes a MarketData object and returns a dataframe containing strike and implied volatility for various expiry dates
+    for the corresponding call options
+    """
+    def helper(implied_vol, row, time):
+        option = EuropeanOption(market_data.spot, row.strike, time, market_data.interest_rate, implied_vol)
+        return option.call_price() - row.marketPrice
+
+    strike_vol = []
+    for date in market_data.expiries:
+        # Time to expiry in years: T in Black Scholes Model
+        time_to_expiry = (dt.strptime(date,"%Y-%m-%d") - market_data.today).days / 365.25
+
+        calls = market_data.get_calls(date)
+
+        for _, data_row in calls.iterrows():
+            try:
+                imp_vol = brentq(f=helper, a=1e-9, b=5, args=(data_row, time_to_expiry)) # Solve for root of helper
+                strike_vol.append((data_row.strike, imp_vol, time_to_expiry))
+            except ValueError:
+                strike_vol.append((data_row.strike, np.nan, time_to_expiry)) # When no solution exists
+
+    strike_vol_df = pd.DataFrame(strike_vol, columns=["strike", "implied_vol", "time_to_expiry"])
+    strike_vol_df = strike_vol_df.dropna().reset_index(drop=True)
+    return strike_vol_df
+
